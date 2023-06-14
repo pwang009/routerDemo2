9253c3d0e1317dccb5d154e634d5e535456e463a
Tony Wang <pwang009@hotmail.com>
2023-06-14 15:52:21 -0700

    hero list does not show

--- src/app/heroes/hero-list/hero-list.component.ts
+++ src/app/heroes/hero-list/hero-list.component.ts
@@ -7,7 +7,7 @@ import { MessageService } from 'src/app/services/message.service';
 @Component({
   selector: 'app-hero-list',
   templateUrl: './hero-list.component.html',
-  styleUrls: ['./hero-list.component.css']
+  styleUrls: ['./hero-list.component.scss']
 })
 export class HeroListComponent implements OnInit {
 
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
